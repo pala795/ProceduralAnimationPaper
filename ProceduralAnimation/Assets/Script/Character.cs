eae895f7c662bffb9dda5c2a923bb7f5a25a2919
Matteo Palagi <mpalagi859@gmail.com>
2026-06-09 16:00:46 +0200

    added animation

--- ProceduralAnimation/Assets/Script/Character.cs
+++ ProceduralAnimation/Assets/Script/Character.cs
@@ -9,6 +9,7 @@ namespace Script
         private Vector2 _moveInput;
         private bool _isMoving;
         [SerializeField] private float maxMovementSpeed;
+        [SerializeField] private Animator _animator;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Awake()
         {
@@ -23,13 +24,16 @@ namespace Script
         {
             if (_moveInput.magnitude != 0)
             {
+                _animator.SetBool("Walking", true);
                 _isMoving = true;
                 Vector3 targetPosition = new Vector3(transform.position.x + _moveInput.x, transform.position.y, transform.position.z + _moveInput.y);
-                transform.position = Vector3.MoveTowards(transform.position, targetPosition, maxMovementSpeed * Time.deltaTime);    
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, maxMovementSpeed * Time.deltaTime);  
+                transform.LookAt(targetPosition, Vector3.up);
             }
             else
             {
                 _isMoving = false;
+                _animator.SetBool("Walking", false);
             }
         }
     }
